113b333667f42d41fc1e21c3237565e8c21486bc
Mark Entingh <entingh@gmail.com>
2022-02-08 04:34:20 -0600

    added uninstall.sql

---
+++ Sql/uninstall.sql
@@ -0,0 +1,19 @@
+﻿DROP TABLE DataSets
+DROP TABLE DataSets_Relationships
+
+DROP SEQUENCE SequenceDataSets
+
+DROP PROCEDURE DataSet_AddRecord
+DROP PROCEDURE DataSet_Create
+DROP PROCEDURE DataSet_Delete
+DROP PROCEDURE DataSet_DeleteRecord
+DROP PROCEDURE DataSet_GetAllColumns
+DROP PROCEDURE DataSet_GetColumns
+DROP PROCEDURE DataSet_GetInfo
+DROP PROCEDURE DataSet_UpdateColumns
+DROP PROCEDURE DataSet_UpdateInfo
+DROP PROCEDURE DataSet_UpdateRecord
+DROP PROCEDURE DataSets_GetList
+DROP PROCEDURE DataSets_Relationship_Create
+DROP PROCEDURE DataSets_Relationships_GetAll
+DROP PROCEDURE DataSets_Relationships_GetList
